096bbf7204e51548dbf28fc0604761935f176276
Johan Markus Steinholt <johan.m.steinholt@gmail.com>
2015-10-05 21:52:50 +0200

    Updated Arduino Code With sensor classes

--- Adruino Code/plant_server/plant_server.ino
+++ Adruino Code/plant_server/plant_server.ino
@@ -1,56 +1,97 @@
 
 #include<Servo.h>
 
-#define trigPin 13
-#define echoPin 12
+class LevelSensor
+{
+  int trigPin;
+  int echoPin;
+  
+  public:
+  
+  LevelSensor(int tPin, int ePin)
+  {
+    trigPin = tPin;
+    echoPin = ePin;
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  }
+  
+  long getLevel()
+  {
+    long duration, distance;
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    return distance;
+  }
+};
+
+//Only skeleton class for now
+class MoistureSensor
+{
+ 
+  int somePin;
+  
+  public:
+  
+  MoistureSensor(int pin1)
+  {
+    somePin = pin1;
+  }
+
+};
+
+class RaspberryPiInterface
+{
+
+  public:
+  
+  RaspberryPiInterface(int bitrate)
+  {
+    Serial.begin(bitrate);
+  }
+  
+  bool avail()
+  {
+    return Serial.available();
+  }
+  
+  int readByte()
+  {
+    return Serial.read();
+  }
+};
+
 
 int command = 0;
 long level = 0;
 
-Servo ser;
+LevelSensor levSens(12, 13);
+MoistureSensor moistSens(11);
+RaspberryPiInterface rpiInterface(9600);
 
-void setup() {
+void setup(){}
 
-  Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-}
-
-void loop() {
-  if (Serial.available()) {
-    command = Serial.read();
+void loop()
+{
+  if (rpiInterface.avail()) {
+    command = rpiInterface.readByte();
     switch (command){
       case 'L':
         //measure level and report
-        level = measureLevel(trigPin, echoPin);
-        Serial.println(level);
+        level = levSens.getLevel();
+        //Serial.println(level);
         break;
       case 'M':
         //measure moisture and report
         break;
       case 'W':
         //water plant and report
-        waterPlant();
         break;
     }
   }
 }
-
-
-long measureLevel(int tPin, int ePin){
-  long duration, distance;
-  digitalWrite(tPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(tPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(tPin, LOW);
-  duration = pulseIn(ePin, HIGH);
-  distance = (duration/2) / 29.1;
-  return distance;
-}
-
-void waterPlant(){
-
-}
-
